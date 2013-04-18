2fffd74dd6775cad27146afe3825402fb8108e1b
Alex <al_x13@hotmail.com>
2013-04-18 09:45:57 +0400

    test

--- railsinstaller_demo/config/routes.rb
+++ railsinstaller_demo/config/routes.rb
@@ -1,6 +1,6 @@
 RailsinstallerDemo::Application.routes.draw do
 
-  get "welcome/index"
+get "welcome/index"
 
 root :to => 'welcome#index'
 
@@ -53,7 +53,7 @@ root :to => 'welcome#index'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 

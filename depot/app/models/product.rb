2a968a38f507ace82a8538e08f6abab4801faab2
Alex <al_x13@hotmail.com>
2013-06-03 22:52:47 +0400

    Playtime

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -2,10 +2,10 @@ class Product < ActiveRecord::Base
 
   attr_accessible :description, :image_url, :price, :title
 
-  default_scope :order => 'title'
-
   has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
+
+  default_scope :order => 'title'
 
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}

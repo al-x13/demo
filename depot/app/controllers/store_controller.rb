43f91878c023a87811f30cdc4e1a7626c50ca2e8
Alex <al_x13@hotmail.com>
2013-06-06 09:43:19 +0400

    Cart creation. Iteration D3. Playtime2

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -4,6 +4,7 @@ class StoreController < ApplicationController
     @products = Product.all
     @time = Time.now
     @counter = user_access_counter
+    @display_counter = "You accessed this page #{@counter} times" if @counter >= 5
   end
 
 end

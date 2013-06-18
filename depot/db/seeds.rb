733aac1d09c896760ee7fc443bb469c9e3750858
Alex <al_x13@hotmail.com>
2013-06-18 11:05:57 +0400

    Refactoring to new book 1

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -23,34 +23,32 @@
 #---
 # encoding: utf-8
 Product.delete_all
-Product.create(:title => 'Web Design for Developers',
-  :description => 
-    %{<p>
-        <em>Web Design for Developers</em> will show you how to make your
-        web-based application look professionally designed. We'll help you
-        learn how to pick the right colors and fonts, avoid costly interface
-        and accessibility mistakes -- your application will really come alive.
-        We'll also walk you through some common Photoshop and CSS techniques
-        and work through a web site redesign, taking a new design from concept
-        all the way to implementation.
+Product.create(title: 'CoffeeScript',
+               description:
+                   %{<p>
+        CoffeeScript is JavaScript done right. It provides all of JavaScript's
+	functionality wrapped in a cleaner, more succinct syntax. In the first
+	book on this exciting new language, CoffeeScript guru Trevor Burnham
+	shows you how to hold onto all the power and flexibility of JavaScript
+	while writing clearer, cleaner, and safer code.
       </p>},
-  :image_url =>   '/images/wd4d.jpg',    
-  :price => 42.95)
+               image_url:   'cs.jpg',
+               price: 36.00)
 # . . .
-Product.create(:title => 'Programming Ruby 1.9',
-  :description =>
-    %{<p>
+Product.create(title: 'Programming Ruby 1.9',
+               description:
+                   %{<p>
         Ruby is the fastest growing and most exciting dynamic language
         out there. If you need to get working programs delivered fast,
         you should add Ruby to your toolbox.
       </p>},
-  :image_url => '/images/ruby.jpg',
-  :price => 49.50)
+               image_url: 'ruby.jpg',
+               price: 49.95)
 # . . .
 
-Product.create(:title => 'Rails Test Prescriptions',
-  :description => 
-    %{<p>
+Product.create(title: 'Rails Test Prescriptions',
+               description:
+                   %{<p>
         <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
         Rails applications, covering Test-Driven Development from both a
         theoretical perspective (why to test) and from a practical perspective
@@ -58,5 +56,5 @@ Product.create(:title => 'Rails Test Prescriptions',
         procedures for Rails 2 and Rails 3, and introduces popular add-ons,
         including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
       </p>},
-  :image_url => '/images/rtp.jpg',
-  :price => 43.75)
+               image_url: 'rtp.jpg',
+               price: 34.95)

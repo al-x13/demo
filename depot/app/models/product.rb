9ee52ee9dd0c659153e190936d487f21e969ebb0
Alex <al_x13@hotmail.com>
2013-06-20 19:50:34 +0400

    Refactoring to new book 2. Completed Iteration F1

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -16,6 +16,8 @@ class Product < ActiveRecord::Base
         :message => 'must be a URL for GIF, JPG or PNG image.'
   }
 
+  private
+
   # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
